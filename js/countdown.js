cdc24a346afb1892c6f0cf8ee3a5d3287b17ea8d
Brandon Fuller <bjcfuller@uri.edu>
2019-11-18 17:11:02 -0500

    Remove console log; fix code styles

--- js/countdown.js
+++ js/countdown.js
@@ -9,7 +9,6 @@
     var countdowns, i;
 
     countdowns = document.querySelectorAll( '.uri-countdown' );
-    console.log( countdowns );
 
     for ( i = 0; i < countdowns.length; i++ ) {
       setupCountdown( countdowns[i] );
@@ -49,25 +48,25 @@
   }
 
   function setCookie(cname, cvalue, exdays) {
-      var d = new Date();
-      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-      var expires = 'expires='+d.toUTCString();
-      document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = 'expires='+d.toUTCString();
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
   }
 
   function getCookie(cname) {
-      var name = cname + '=';
-      var ca = document.cookie.split(';');
-      for(var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == ' ') {
-              c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-              return c.substring(name.length, c.length);
-          }
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
       }
-      return '';
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
   }
 
 })();
